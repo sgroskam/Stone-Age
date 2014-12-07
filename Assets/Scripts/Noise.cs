9683071af8e0872b302256d8676bcd9a6fc3a50e
Schuyler G Roskam <sgroskam@gmail.com>
2014-12-06 17:57:15 -0700

    Procedural textures work well. Started work on value noise.

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Noise
+{
+	private static int[] hash = {0, 1, 2, 3, 4, 5, 6, 7};
+	public static float Value( Vector3 point, float frequency)
+	{
+		point *= frequency;
+		int i = Mathf.FloorToInt(point.x);
+		i &= 7;
+		return hash[i] / 7f;
+	}
+}
--- Assets/Scripts/TextureCreator.cs
+++ Assets/Scripts/TextureCreator.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TextureCreator : MonoBehaviour {
+public class TextureCreator : MonoBehaviour 
+{
+	[Range(2, 512)]
+	public int resolution = 256;
+	public float frequency = 1.0f;
+	private Texture2D texture;
 
-	// Use this for initialization
-	void Start () {
-	
+	private void OnEnable()
+	{
+		if(texture == null)
+		{
+			texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, false);
+			texture.name = "Procedural Texture";
+			texture.wrapMode = TextureWrapMode.Clamp;
+			texture.filterMode = FilterMode.Trilinear;
+			texture.anisoLevel = 9;
+			GetComponent<MeshRenderer>().material.mainTexture = texture;
+		}
+		FillTexture();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void Update()
+	{
+		if(transform.hasChanged)
+		{
+			transform.hasChanged = false;
+			FillTexture();
+		}
 	}
+
+	public void FillTexture()
+	{
+		if(texture.width != resolution)
+		{
+			texture.Resize(resolution, resolution);
+		}
+
+		//pointxy; 0 is left or bottom; 1 is right or top.
+		Vector3 point00 = transform.TransformPoint( new Vector3(-0.5f, -0.5f));
+		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+		Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+		Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+		
+		float stepSize = 1.0f / resolution;
+		for(int y = 0; y < resolution; y++)
+		{
+			//Find the left and right edges.
+			Vector3 point0 = Vector3.Lerp( point00, point01, (( y + 0.5f) * stepSize));
+			Vector3 point1 = Vector3.Lerp( point10, point11, (( y + 0.5f) * stepSize));
+
+			for(int x = 0; x < resolution; x++)
+			{
+				//cycle through points in each row of the texture.
+				Vector3 point = Vector3.Lerp( point0, point1, ( x + 0.5f) * stepSize);
+
+				texture.SetPixel( x, y, Color.white * Noise.Value(point, frequency));
+			}
+		}
+		texture.Apply();
+	}
+
+
 }
