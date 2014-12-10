22d8313e39c863a176caf5d97247d3c1a89120ca
Schuyler G Roskam <sgroskam@gmail.com>
2014-12-10 00:42:03 -0700

    Added LibNoise. Started testing mouse control and movement of units.

---
+++ Assets/Scripts/ClickData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickData : MonoBehaviour 
+{
+
+	RaycastHit mouseHit;
+	public Vector3 mousePoint = new Vector3(0.0f, 0.0f, 0.0f);
+	
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(1))
+		{
+			Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Physics.Raycast(mouseRay, out mouseHit);
+			mousePoint = mouseHit.point;
+		}
+		
+		Debug.Log("x: " + mouseHit.point.x + "\tz: " + mouseHit.point.z);
+	}
+}
---
+++ Assets/Scripts/PathTo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathTo : MonoBehaviour 
+{
+	public GameObject mouseManager;
+
+	private ClickData clickData;
+	private Vector3 mousePoint;
+
+	void Awake()
+	{
+		clickData = mouseManager.GetComponent<ClickData>();
+	}
+
+	void Update()
+	{
+		if(mousePoint != clickData.mousePoint)
+		{
+			mousePoint = clickData.mousePoint;
+			transform.position = mousePoint + new Vector3( 0.0f, 1.0f, 0.0f);
+		}
+	}
+}
